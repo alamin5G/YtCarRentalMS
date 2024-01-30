407200e6e8b9267d3b01bae05253d236bfa9fce2
alamin5g <alaminrakib4864@gmail.com>
2024-01-30 21:40:37 +0600

    init

--- src/com/goonok/Car.java
+++ src/com/goonok/Car.java
@@ -1,2 +1,17 @@
-package com.goonok;public class Car {
+package com.goonok;
+
+public class Car {
+    private String carId;
+    private String carModel;
+    private String brand;
+    private double basePricePerDay;
+    private boolean isCarAvailable;
+
+    public Car(String carId, String carModel, String brand, double basePricePerDay) {
+        this.carId = carId;
+        this.carModel = carModel;
+        this.brand = brand;
+        this.basePricePerDay = basePricePerDay;
+        isCarAvailable = true;
+    }
 }
